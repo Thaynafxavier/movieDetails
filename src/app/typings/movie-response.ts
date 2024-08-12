84ace91580ef1a3c9eebafa1c3dd141bec2a8935
Thayna Fernandes <thaynafxavier@hotmail.com>
2024-08-12 10:32:02 -0300

    integracao da parte das informacoes sobre o filme

--- src/app/typings/movie-response.ts
+++ src/app/typings/movie-response.ts
@@ -62,3 +62,102 @@ export interface CertificationData {
   certification: string;
   release_date: string;
 }
+
+export interface ReleaseDate {
+  certification: string;
+  release_date: string;
+  type: number;
+  // Outras propriedades, caso existam
+}
+
+export interface ReleaseInfo {
+  iso_3166_1: string;
+  release_dates: ReleaseDate[];
+}
+
+export interface ApiResponse {
+  id: number;
+  results: ReleaseInfo[];
+}
+
+export interface Genre {
+  id: number;
+  name: string;
+}
+
+
+export interface CreditsResponse {
+  id: number;
+  cast: CastMember[];
+  crew: CrewMember[];
+}
+
+export interface CastMember {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: string;
+  cast_id: number;
+  character: string;
+  credit_id: string;
+  order: number;
+}
+
+export interface CrewMember {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: string;
+  credit_id: string;
+  department: string;
+  job: string;
+}
+
+export interface TranslationData {
+  homepage: string;
+  overview: string;
+  runtime: number;
+  tagline: string;
+  title: string;
+}
+
+interface MovieTranslation {
+  iso_639_1: string;
+  data: {
+    overview: string;
+    // outras propriedades
+  };
+}
+
+export interface MovieResponse {
+  translations?: MovieTranslation[];
+  // outras propriedades
+}
+export interface MovieTranslationsResponse {
+  id: number;
+  translations: Translation[];
+}
+
+export interface Translation {
+  iso_3166_1: string;
+  iso_639_1: string;
+  name: string;
+  english_name: string;
+  data: TranslationData;
+}
+
+export interface TranslationData {
+  homepage: string;
+  overview: string;
+  runtime: number;
+  tagline: string;
+  title: string;
+}
